c680d4e8a745148b14c8c100ef21333722f85461
phtrebil <phptrebil@gmail.com>
2023-07-25 17:45:56 -0300

    aprendendo mais sobre composable e previews

--- app/src/main/java/com/example/alluvery/MainActivity.kt
+++ app/src/main/java/com/example/alluvery/MainActivity.kt
@@ -3,44 +3,35 @@ package com.example.alluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.alluvery.ui.theme.AlluveryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AlluveryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyFirstComposeble()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyFirstComposeble(){
+    Column(Modifier.background(color = Color.White)) {
+        Text(text = "Meu primeiro texto. ")
+        Text(text = "Meu segundo texto.")
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    AlluveryTheme {
-        Greeting("Android")
-    }
+fun MyFirstComposevlePreview(){
+    MyFirstComposeble()
 }
