da5f4b89073542445ec6d8b0f69e66eecd2dcd1d
phtrebil <phptrebil@gmail.com>
2023-07-26 20:32:38 -0300

    utilizando column e row em conjunto

--- app/src/main/java/com/example/alluvery/MainActivity.kt
+++ app/src/main/java/com/example/alluvery/MainActivity.kt
@@ -6,26 +6,40 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.alluvery.ui.theme.AlluveryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyFirstComposeble()
+            AlluveryTheme{
+                Surface{
+                    MyFirstComposeble()
+                }
+                
+            }
+
         }
     }
 }
 
 @Composable
 fun MyFirstComposeble(){
-    Column(Modifier.background(color = Color.White)) {
-        Text(text = "Meu primeiro texto. ")
-        Text(text = "Meu segundo texto.")
+    Row{
+        Column(Modifier.background(color = Color.White)) {
+            Text(text = "Meu primeiro texto. ")
+            Text(text = "Meu segundo texto.")
+        }
+        Column(Modifier.background(color = Color.White)) {
+            Text(text = "Meu terceiro texto. ")
+            Text(text = "Meu Quarto texto.")
+        }
     }
 
 }
