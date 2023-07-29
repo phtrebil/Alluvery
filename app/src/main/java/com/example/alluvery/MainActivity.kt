8c3be4132c80c5d4300c28d74bd7dc0c8f970773
phtrebil <phptrebil@gmail.com>
2023-07-29 16:39:09 -0300

    trabalhando no box do produto

--- app/src/main/java/com/example/alluvery/MainActivity.kt
+++ app/src/main/java/com/example/alluvery/MainActivity.kt
@@ -7,17 +7,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.alluvery.ui.theme.AlluveryTheme
+import com.example.alluvery.ui.theme.Purple500
+import com.example.alluvery.ui.theme.Teal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,11 +44,22 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showSystemUi = true)
 @Composable fun PrimeiroProduto(){
-    Column() {
-        Box(modifier = Modifier.height(50.dp).width(50.dp)
-            .background(color = Color.Cyan))
+    Column(
+        Modifier
+            .height(250.dp)
+            .width(200.dp)
+    ) {
+        Box(modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth()
+            .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal))))
         Image(painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "fundo verde quadriculado")
+            contentDescription = "fundo verde quadriculado",
+        Modifier.size(100.dp)
+            .offset(y = (-50).dp)
+            .clip(shape = CircleShape)
+            .align(CenterHorizontally)
+            )
         Text(text = "testo 1")
         Text(text = "testo 2")
     }
--- app/src/main/java/com/example/alluvery/ui/theme/Color.kt
+++ app/src/main/java/com/example/alluvery/ui/theme/Color.kt
@@ -3,8 +3,11 @@ package com.example.alluvery.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
+val Purple500 = Color(0xFF6200EE)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+
+val Teal = Color(0xFF03dac5)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
