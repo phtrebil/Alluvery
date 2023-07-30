eb37a41a43eb7bcde71e1b28d9d95b8ca833f41f
phtrebil <phptrebil@gmail.com>
2023-07-30 10:04:16 -0300

    formatando o texto do nome e preço do produto

--- app/src/main/java/com/example/alluvery/MainActivity.kt
+++ app/src/main/java/com/example/alluvery/MainActivity.kt
@@ -7,22 +7,28 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.alluvery.ui.theme.AlluveryTheme
 import com.example.alluvery.ui.theme.Purple500
 import com.example.alluvery.ui.theme.Teal
@@ -42,7 +48,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showSystemUi = true)
+@Preview(showBackground = true)
 @Composable fun PrimeiroProduto(){
     Column(
         Modifier
@@ -52,15 +58,28 @@ class MainActivity : ComponentActivity() {
         Box(modifier = Modifier
             .height(100.dp)
             .fillMaxWidth()
-            .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal))))
-        Image(painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "fundo verde quadriculado",
-        Modifier.size(100.dp)
-            .offset(y = (-50).dp)
-            .clip(shape = CircleShape)
-            .align(CenterHorizontally)
+            .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal)))){
+            Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "fundo verde quadriculado",
+                Modifier
+                    .size(100.dp)
+                    .offset(y = 50.dp)
+                    .clip(shape = CircleShape)
+                    .align(BottomCenter)
             )
-        Text(text = "testo 1")
-        Text(text = "testo 2")
+        }
+        Spacer(Modifier.height(50.dp))
+
+        Column(Modifier.padding(16.dp)) {
+            Text(text = LoremIpsum(500).values.first(),
+                fontSize = 18.sp,
+                fontWeight = FontWeight(700),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Text(text = "R$14.99", Modifier.padding(top = 8.dp) ,  fontSize = 14.sp, fontWeight = FontWeight(400))
+        }
+
     }
 }
