1b64a3f5feed9d0fffb3668fd4d59c0df7d2872b
phtrebil <phptrebil@gmail.com>
2023-08-15 16:32:13 -0300

    estudando estados para melhorar a searchBar

--- app/src/main/java/com/example/alluvery/ui/components/SearchTextField.kt
+++ app/src/main/java/com/example/alluvery/ui/components/SearchTextField.kt
@@ -1,15 +1,20 @@
 package com.example.alluvery.ui.components
 
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun SearchBar(){
-    OutlinedTextField(value = "Pedro", onValueChange = {})
+    var text by remember{ mutableStateOf("Pedro")}
+    OutlinedTextField(value = text, onValueChange = {newValue ->
+        text = newValue;
+    })
 }
