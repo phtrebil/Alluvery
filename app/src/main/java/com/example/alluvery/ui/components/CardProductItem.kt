7e9ba2e9253846af0c3485e89cf6514e0b0437c5
phtrebil <phptrebil@gmail.com>
2023-08-27 19:46:31 -0300

    adicionando expansão do descrição ao clicar no item

--- app/src/main/java/com/example/alluvery/ui/components/CardProductItem.kt
+++ app/src/main/java/com/example/alluvery/ui/components/CardProductItem.kt
@@ -1,6 +1,7 @@
 package com.example.alluvery.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,12 +11,18 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.alluvery.R
@@ -28,12 +35,20 @@ import java.math.BigDecimal
 @Composable
 fun CardProductItem(
     product: Product,
-    modifier: Modifier = Modifier
+    elevation: Dp = 4.dp,
+    modifier: Modifier = Modifier,
+    expanded: Boolean = false
 ) {
+    var expandedState by remember {
+        mutableStateOf(expanded)
+    }
     Card(
         modifier
             .fillMaxWidth()
             .heightIn(150.dp)
+            .clickable {
+                expandedState = !expandedState
+            }
     ) {
         Column() {
             AsyncImage(
@@ -61,11 +76,14 @@ fun CardProductItem(
                     color = Color.White
                 )
             }
+
             if(product.descricao != ""){
                 Text(
                     text = product.descricao,
                     Modifier
-                        .padding(16.dp)
+                        .padding(16.dp),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2
                 )
             }
         }
