34aa5966cbde577dba59f2b2b486e5fc199de5c8
phtrebil <phptrebil@gmail.com>
2023-08-29 16:50:06 -0300

    melhorando função de busca

--- app/src/main/java/com/example/alluvery/ui/components/HomeScreen.kt
+++ app/src/main/java/com/example/alluvery/ui/components/HomeScreen.kt
@@ -1,21 +1,20 @@
-package com.example.alluvery.ui.components
+package br.com.alura.aluvery.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.alluvery.model.Product
 import com.example.alluvery.sampledata.sampleProducts
 import com.example.alluvery.sampledata.sampleSections
+import com.example.alluvery.ui.components.CardProductItem
+import com.example.alluvery.ui.components.ListaDeProdutos
+import com.example.alluvery.ui.components.SearchBar
 import com.example.alluvery.ui.theme.AlluveryTheme
+
 
 @Composable
 fun HomeScreen(
@@ -23,20 +22,77 @@ fun HomeScreen(
     searchText: String = ""
 ) {
     Column {
-        SearchBar(searchText)
+        var text by remember {
+            mutableStateOf(searchText)
+        }
+        SearchBar(
+            searchText = text,
+            onSearchChange = {
+                text = it
+            },
+            Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+        )
+        val searchedProducts = remember(text) {
+            if (text.isNotBlank()) {
+                sampleProducts.filter { product ->
+                    product.nome.contains(
+                        text,
+                        ignoreCase = true,
+                    ) ||
+                            product.descricao?.contains(
+                                text,
+                                ignoreCase = true,
+                            ) ?: false
+                }
+            } else emptyList()
+        }
         LazyColumn(
             Modifier
                 .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             contentPadding = PaddingValues(bottom = 16.dp)
         ) {
-            items(sampleProducts) { p ->
-                CardProductItem(
-                    product = p,
-                )
+            if (text.isBlank()) {
+                for (section in sections) {
+                    val title = section.key
+                    val products = section.value
+                    item {
+                        ListaDeProdutos(titulo = title, products = products)
+                    }
+                }
+            } else {
+                items(searchedProducts) { p ->
+                    CardProductItem(
+                        product = p,
+                        Modifier.padding(horizontal = 16.dp)
+                    )
+                }
             }
         }
     }
 }
 
+@Preview(showSystemUi = true)
+@Composable
+private fun HomeScreenPreview() {
+    AlluveryTheme {
+        Surface {
+            HomeScreen(sampleSections)
+        }
+    }
+}
 
+@Preview
+@Composable
+fun HomeScreenWithSearchTextPreview() {
+    AluveryTheme {
+        Surface {
+            HomeScreen(
+                sampleSections,
+                searchText = "a",
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/alluvery/ui/components/SearchTextField.kt
+++ app/src/main/java/com/example/alluvery/ui/components/SearchTextField.kt
@@ -1,11 +1,5 @@
 package com.example.alluvery.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -14,30 +8,24 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
 @Composable
-fun SearchBar(searchText: String = ""){
-    var text by remember{ mutableStateOf(searchText)}
+fun SearchBar(
+    searchText: String,
+    onSearchChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
     OutlinedTextField(
-        value = text,
+        value = searchText,
         onValueChange = { newValue ->
-            text = newValue
+            onSearchChange(newValue)
         },
-        Modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(25),
+        modifier,
+        shape = RoundedCornerShape(100),
         leadingIcon = {
-            Icon(Icons.Default.Search, contentDescription = "icone de busca")
+            Icon(Icons.Default.Search, contentDescription = "ícone de lupa")
         },
         label = {
             Text(text = "Produto")
@@ -47,22 +35,4 @@ fun SearchBar(searchText: String = ""){
         }
 
     )
-    LazyColumn(
-            Modifier
-                .fillMaxSize(),
-    verticalArrangement = Arrangement.spacedBy(16.dp),
-    contentPadding = PaddingValues(bottom = 16.dp)
-    ){
-        if(text.isBlank()) {
-            for (section in sections) {
-                val title = section.key
-                val products = section.value
-                item {
-                    ProductsSection(
-                        title = title,
-                        products = products
-                    )
-                }
-            }
-    }
 }
