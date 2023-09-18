fc24a65516f2dce55610b8bf041d139d3fdbf49a
phtrebil <phptrebil@gmail.com>
2023-09-18 11:26:17 -0300

    finalizando filtro da searchbar

--- app/src/main/java/com/example/alluvery/MainActivity.kt
+++ app/src/main/java/com/example/alluvery/MainActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import br.com.alura.aluvery.ui.screens.HomeScreen
 import com.example.alluvery.sampledata.sampleSections
-import com.example.alluvery.ui.components.HomeScreen
 import com.example.alluvery.ui.theme.AlluveryTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/alluvery/ui/components/HomeScreen.kt
+++ app/src/main/java/com/example/alluvery/ui/components/HomeScreen.kt
@@ -1,9 +1,19 @@
 package br.com.alura.aluvery.ui.screens
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -66,13 +76,14 @@ fun HomeScreen(
                 items(searchedProducts) { p ->
                     CardProductItem(
                         product = p,
-                        Modifier.padding(horizontal = 16.dp)
+                        modifier = Modifier.padding(horizontal = 16.dp)
                     )
                 }
             }
         }
     }
 }
+
 
 @Preview(showSystemUi = true)
 @Composable
@@ -87,7 +98,7 @@ private fun HomeScreenPreview() {
 @Preview
 @Composable
 fun HomeScreenWithSearchTextPreview() {
-    AluveryTheme {
+    AlluveryTheme {
         Surface {
             HomeScreen(
                 sampleSections,
