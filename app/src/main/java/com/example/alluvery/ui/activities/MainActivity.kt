4275545dfa205d3fe91e2c387a4d354dc1163bce
phtrebil <phptrebil@gmail.com>
2023-09-29 10:36:34 -0300

    Adicionando fab, levando da tela de lista de produtos até a tela de cadastro de produtos

--- app/src/main/java/com/example/alluvery/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/alluvery/ui/activities/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.example.alluvery.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import br.com.alura.aluvery.ui.screens.HomeScreen
 import com.example.alluvery.sampledata.sampleSections
 import com.example.alluvery.ui.theme.AlluveryTheme
@@ -12,11 +23,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AlluveryTheme {
-                Surface {
-                    HomeScreen(sampleSections)
-                }
+            App(onFabClick = {
+                startActivity(Intent(this, ProductFormActivity::class.java))
+            })
 
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun App(onFabClick: () -> Unit) {
+        AlluveryTheme {
+            Surface {
+                Scaffold(floatingActionButton = {
+                    FloatingActionButton(onClick = onFabClick) {
+                        Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                    }
+                }) { paddingValues ->
+                    Box(Modifier.padding(paddingValues)) {
+                        HomeScreen(sampleSections)
+                    }
+                }
             }
 
         }
